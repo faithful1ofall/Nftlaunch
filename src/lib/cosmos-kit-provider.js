d9fdeddfd336fe9279ec897ffd48fb7dabd2cdbf
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-03-11 00:24:44 +0100

    Update cosmos-kit-provider.js

--- src/lib/cosmos-kit-provider.js
+++ src/lib/cosmos-kit-provider.js
@@ -3,7 +3,8 @@
 import "@interchain-ui/react/styles";
 import { ChainProvider } from "@cosmos-kit/react";
 import { chains, assets } from "chain-registry";
-import { wallets } from "@cosmos-kit/keplr";
+//import { wallets } from "@cosmos-kit/keplr";
+import { wallets } from 'cosmos-kit';
 
 export function CosmosKitProvider({ children }) {
   return (
