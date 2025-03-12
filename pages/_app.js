e465b25409fa1e788244d70683747057e5d991d3
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-03-12 16:19:20 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,23 @@ import ContextProvider from "src/utils/ContextProvider";
 import GlobalStyles from "@assets/styles/GlobalStyles";
 import { CosmosKitProvider } from "src/lib/cosmos-kit-provider";
 import { ShuttleProvider } from "@delphi-labs/shuttle-react"
+import { INJECTIVE_MAINNET,  INJECTIVE_TESTNET } from 'src/lib/chains';
+
+import { wallets } from "@cosmos-kit/keplr";
+//import { wallets } from 'cosmos-kit';
 
 const App = ({ Component, pageProps }) => {
   const [showChild, setShowChild] = useState(false);
+
+  const extensionProviders = [
+  new KeplrExtensionProvider({
+    networks: [INJECTIVE_MAINNET,  INJECTIVE_TESTNET]
+  }),
+  new MetamaskExtensionProvider({
+    networks: [INJECTIVE_MAINNET,  INJECTIVE_TESTNET]
+  })
+];
+
 
   useEffect(() => {
     setShowChild(true);
@@ -16,7 +30,10 @@ const App = ({ Component, pageProps }) => {
   }
 
   return (
-    <ShuttleProvider>
+    <ShuttleProvider
+      extensionProviders={extensionProviders}
+      persistent
+      >
     <ContextProvider>
       <GlobalStyles />
       <Component {...pageProps} />
