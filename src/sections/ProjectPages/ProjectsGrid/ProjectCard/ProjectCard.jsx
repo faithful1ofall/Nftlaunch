7f1783892b0f62a1eda79e2e634dc6b5879967a7
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-03-16 08:01:28 +0100

    Update ProjectCard.jsx

--- src/sections/ProjectPages/ProjectsGrid/ProjectCard/ProjectCard.jsx
+++ src/sections/ProjectPages/ProjectsGrid/ProjectCard/ProjectCard.jsx
@@ -23,7 +23,7 @@ const mintnft = async () => {
   setLoading(true); 
 
   try {
-    let phase = [];
+   /* let phase = [];
 
     if (phase.length == 0) {
     const currentTime = Math.floor(Date.now() / 1000); 
@@ -37,7 +37,7 @@ const mintnft = async () => {
         end_time: currentTime + oneYearInSeconds, 
     };
     phase.push(new_phase);
-}
+}*/
       
 /*    const msg = new MsgExecuteContract({
     sender: recentWallet.account.address,
@@ -49,7 +49,7 @@ const mintnft = async () => {
     },
   });*/
 
-    const msg1 = new MsgExecuteContract({
+  /*  const msg1 = new MsgExecuteContract({
     sender: recentWallet.account.address,
     contract: address,
     msg: {
@@ -57,7 +57,7 @@ const mintnft = async () => {
         mint_phase: phase
       }
     },
-  });
+  });*/
   let extensions = [{}]
     console.log('totsupply', projectDetails[0].text);
 
@@ -81,9 +81,9 @@ const mintnft = async () => {
   });
 
       const msgs = [msg];
-   const msgs1 = [msg1];
+  // const msgs1 = [msg1];
     console.log('msgs', msgs);
-  console.log('msgs', msgs1);
+ // console.log('msgs', msgs1);
 
       const response = await simulate({
     messages: msgs,
@@ -93,14 +93,14 @@ const mintnft = async () => {
    const  feeest = response.fee?.amount[0];
    const  gasLimit = response.fee?.gas;
     
-const result1 = await broadcast({
+/*const result1 = await broadcast({
                 wallet: recentWallet,
                 messages: msgs1,
                 feeAmount: feeest?.amount,
                 gasLimit: gasLimit,
             });
     console.log("1st Transaction successful:", result1);
-  
+  */
 
       const result = await broadcast({
                 wallet: recentWallet,
